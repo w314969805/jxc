4ad2a0d630f07e2ab5bd9f15886085fc1053053b
w314969805 <w314969805@gmai.com>
2016-12-29 00:37:59 +0800

    测试
    测试2016-12-29

--- src/com/jxc/robot/dao/UserDao.java
+++ src/com/jxc/robot/dao/UserDao.java
@@ -5,5 +5,6 @@ public class UserDao {
 	public static void main(String[] args) {
 		System.out.println("Hello gitHub");
 		System.out.println("***************************");
+		System.out.println("***************************");
 	}
 }
